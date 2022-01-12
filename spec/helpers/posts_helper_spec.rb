64a2441c806240d0d7bb5c5a13be59bf57ee49ec
guk0 <abcgy39@naver.com>
2022-01-12 09:25:23 +0900

    fix request spec / add helper spec

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+include PostsHelper
 # Specs in this file have access to a helper object that includes
 # the PostsHelper. For example:
 #
@@ -11,5 +11,16 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe PostsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'assigns a user to post' do
+    # AAA(Arrange, Act, Assert)
+    # Arranging our data -> Assigning variables
+
+    creator = User.first_or_create(email: 'cgycgy39@gmail.com', password: "password", password_confirmation: "password")
+    @post = Post.new(title: "MyString", body: "MyText", views: 1)
+    # Act => We're acting with our arranged variables
+    returned_post = assign_post_creator(@post, creator)
+    
+    #Assert => Assert that our expected outcome matches our actual outcome.
+    expect(returned_post.user).to be(creator)
+  end
 end
